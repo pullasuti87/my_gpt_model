e2629a6f5f85e7702cdd597f9de5d846cd1104ea
pullasuti87 <pullasuti87@gmail.com>
2024-08-03 20:50:38 +0300

    mapping chars

--- main.py
+++ main.py
@@ -4,5 +4,23 @@ import torch
 
 data = open("dataset.txt", "r")
 dataset = data.read()
-print(dataset)
-print(len(dataset))
+# print(dataset)
+# print(len(dataset))
+
+chars = sorted(set(dataset))
+# print(chars)
+
+# chars to int
+str_to_int = {}
+for i, c in enumerate(chars):
+    str_to_int[c] = i
+
+
+# int to char
+int_to_str = {}
+for i, c in enumerate(chars):
+    int_to_str[i] = c
+
+
+print(str_to_int, end="\n\n")
+print(int_to_str)
