7a633147f6a0c9e1f4fcfc807639b137a32fba19
pullasuti87 <pullasuti87@gmail.com>
2024-08-12 23:50:31 +0300

    encode/decode

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # pytorch more familiar, haven't been using tensorflow
-import torch
+# import torch
 
 
 data = open("dataset.txt", "r")
@@ -10,17 +10,37 @@ dataset = data.read()
 chars = sorted(set(dataset))
 # print(chars)
 
-# chars to int
+# TOKENIZATION
+
+# str to int
 str_to_int = {}
-for i, c in enumerate(chars):
-    str_to_int[c] = i
-
-
-# int to char
+# int to str
 int_to_str = {}
 for i, c in enumerate(chars):
+    str_to_int[c] = i
     int_to_str[i] = c
 
 
-print(str_to_int, end="\n\n")
-print(int_to_str)
+def encode_chars(s):
+    t = []
+    for c in s:
+        t.append(str_to_int[c])
+    return t
+
+
+def decode_ints(ints):
+    s = ""
+    for i in ints:
+        s += int_to_str[i]
+    return s
+
+
+s = "my name is slim Shady"
+print(s)
+test = encode_chars(s)
+print(test)
+
+print(decode_ints(test))
+
+# print(str_to_int, end="\n\n")
+# print(int_to_str)
